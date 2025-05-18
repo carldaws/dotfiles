c47af96658270e54bb34f8f9511207a969f5211d
Carl Dawson <carldawson@hey.com>
2025-05-18 22:27:43 +0100

    Adds surface keybind for lazgit for dotfiles

--- .config/nvim/lua/carldaws/plugins/surface.lua
+++ .config/nvim/lua/carldaws/plugins/surface.lua
@@ -3,7 +3,8 @@ return {
 	opts = {
 		default_position = "right",
 		mappings = {
-			{ keymap = "lg", command = "lazygit",                      position = "center" },
+			{ keymap = "lg", command = "lazygit",                                             position = "center" },
+			{ keymap = ".g", command = "GIT_DIR=$HOME/.dotfiles GIT_WORK_TREE=$HOME lazygit", position = "center" },
 			{ keymap = "ld", command = "lazydocker" },
 			{ keymap = "rc", command = "rails console" },
 			{ keymap = "db", command = "psql -h localhost -U postgres" },
